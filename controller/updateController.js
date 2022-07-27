0e5cb1fac0cb01ed2d642cff9a71838f258e9294
Michael <kuechler.michael03@gmail.com>
2022-07-27 09:49:54 +0200

    refactor

--- controller/updateController.js
+++ controller/updateController.js
@@ -1,26 +1,24 @@
 const mongoose = require('mongoose');
 const User = require('./../User.js');
 
-const completeStatusList = ["idle", "atQuest", "endQuest", "atEvent", "atTraining", "unconscious"];
+//const completeStatusList = ["idle", "atQuest", "endQuest", "atEvent", "atTraining", "unconscious"];
 const statusWithEndTime = ["atQuest", "atEvent", "atTraining", "unconscious"];
 
-const checkForUpdates = async ()=>{
-    //get all user with the wanted status and status time ended
-    const results = await User.find({status: {$in:statusWithEndTime}, status_time: {$lte: Date.now()}});
-    for (let x in results){
+const checkForUpdates = async () => {
+    //get all user with the wanted status and status time ended every x seconds
+    const results = await User.find({ status: { $in: statusWithEndTime }, status_time: { $lte: Date.now() } });
+    for (let x in results) {
         const user = results[x];
-        try{
+        try {
             //set action based on state
-            switch(user.status){
+            switch (user.status) {
                 case "atQuest":
                     //set status to endQuest
-                    console.log(`${user.status}: atQuest`);
                     user.status = "endQuest";
                     await user.save();
                     break;
 
                 case "atEvent":
-                    console.log(`${user.status}: atEvent`);
                     //set status to idle, give event rewards
                     user.status = "idle";
                     //TODO: give event rewards
@@ -28,7 +26,6 @@ const checkForUpdates = async ()=>{
                     break;
 
                 case "atTraining":
-                    console.log(`${user.status}: atTraining`);
                     //set status to idle, give training rewards
                     user.status = "idle";
                     //TODO: give training rewards
@@ -43,21 +40,16 @@ const checkForUpdates = async ()=>{
                     await user.save();
                     break;
             }
-            
+
         }
-        catch{
+        catch {
             //change state to idle if something goes wrong
             console.log(`error in updateController!!`);
             user.status = "idle";
             await user.save();
         }
     }
-    setTimeout(checkForUpdates, 1000 * 30);
+    setTimeout(checkForUpdates, 1000 * 10);
 
 }
-
-const updateHealth = (user) =>{
-    console.log(`call updateHealth on user ${user.tag}`);}
-    //How healing works: Heal with an item. Heal with /heal for 30%max health once a day. No healing over time. No need for this function!
-
-module.exports={checkForUpdates};
+module.exports = { checkForUpdates };
