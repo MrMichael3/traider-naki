81cbeeb460b81aed59055488d5ab3c82e802383e
Michael <kuechler.michael03@gmail.com>
2022-07-28 10:03:07 +0200

    fixed formula

--- controller/unitLevel.js
+++ controller/unitLevel.js
@@ -6,7 +6,7 @@ const emojis = require('./../emojis.json');
 
 const baseXp = 350 //xp for level 0;
 const multiplier = 1.2;
-const levelOne = baseXp * multiplier - baseXp; //xp needed for first lv up
+const levelOne = 0; //xp needed for first lv up
 const statsMultiplier = 20; //increase health and attack per level
 
 function createEmbeds(userBefore, userAfter) {
@@ -30,20 +30,21 @@ function createEmbeds(userBefore, userAfter) {
     return embeds;
 }
 const xpOfLevel = (lv) => {
-    return (baseXp * Math.pow(multiplier, lv) - baseXp);
+    return (baseXp * Math.pow(multiplier, lv - 1) - baseXp);
 }
 const getUnitLevel = (xp) => {
     //calculate level, each unit has same xp rate, min lv: 1
     if (xp < levelOne) {
         return 1;
     }
-    return (Math.floor(Math.log((baseXp + xp) / baseXp) / Math.log(multiplier)));
+    return (Math.floor(Math.log((baseXp + xp) / baseXp) / Math.log(multiplier)) + 1);
 }
 const levelUp = async (user, xpBefore, channel) => {
     //increase hp and attack by 20% each level
     //formula: health = baseHealth (lv1) * ((100+20)/100)^level
     const xp = user.unit.xp;
     const lv = getUnitLevel(xp);
+    const previousLv = getUnitLevel(xpBefore);
     const userBefore = {};
     const unitType = user.unit.unit_type;
     userBefore['max_health'] = user.unit.max_health;
@@ -54,8 +55,8 @@ const levelUp = async (user, xpBefore, channel) => {
     const previousHealth = user.unit.max_health;
     const previousMinAttack = user.unit.min_attack;
     const previousMaxAttack = user.unit.max_attack;
-    if (lv <= 1) {
-        console.log(`error: unit lv ${lv} hasn't leveled up!`);
+    if (lv <= previousLv) {
+        console.log(`error: ${user.username} lv ${lv} hasn't leveled up!`);
         return;
     }
     switch (user.unit.unit_type) {
