54eb7928f52b78c981151a026d66b093ed38d4c5
Michael <kuechler.michael03@gmail.com>
2022-08-09 10:32:01 +0200

    add shop command

---
+++ commands/shop.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose');
+const User = require('./../User.js');
+const Item = require('./../schemas/Item.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+
+function createEmbeds() {
+    // An embed message with the shop and all available items
+    const embeds = [];
+    var thumbnail = "";
+    const availableItems = [{ name: '**Consumables**', value: `Heal Potions, Snacks and more` }];
+    //iterate through items and add to the array
+    for await (const item of Item.find({ consumable: true })) {
+        availableItems.push({ name: `${item.name} - ${item.cost} Soulstones`, value: `${item.description}`, inline: true });
+    }
+    if (await Item.find({ consumable: false })) {
+        availableItems.push({ name: '**Static Items**', value: `Artifacts and other Collectibles` });
+        for await (const item of Item.find({ consumable: false })) {
+            //iterate through items and add to the array
+            availableItems.push({ name: `${item.name} - ${item.cost} Soulstones`, value: `${item.description}`, inline: true });
+        }
+    }
+    if (availableItems.length > 25) {
+        console.log("Too many items in shop!");
+        availableItems = availableItems.slice(0, 24);
+    }
+    const shopEmbed = new MessageEmbed()
+        .setTitle(`Traider Naki's Shop`)
+        .setThumbnail(thumbnail)
+        .setDescription(`Spend your Soulstones on consumables and static items. \n Use '/shop [item] to get more information and '/buy [item] to buy an item.`)
+        .addFields(availableItems)
+    embeds.push(shopEmbed);
+    return embeds;
+}
+function createItemEmbeds(item) {
+    // An embed message with details of selected item
+    const embeds = [];
+    var itemType = "Static";
+    if (item.consumable) {
+        itemType = "Consumable";
+    }
+    const itemEmbed = new MessageEmbed()
+        .setTitle(`${item.name}`)
+        .setDescription(`${item.description}`)
+        .setThumbnail(item.image)
+        .addFields(
+            { name: `Price`, value: `${item.cost} Soulstones`, inline: true },
+            { name: `Type`, value: `${itemType}`, inline: true }
+        )
+    embeds.push(itemEmbed);
+    return embeds;
+}
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('shop')
+        .setDescription('Buy items here')
+        .addStringOption((option) => option
+            .setName('item')
+            .setDescription('item name')),
+    async execute(interaction) {
+        //shop [item]
+        if (interaction.options.get('item')) {
+            const itemName = interaction.options.get('item');
+            //check if item exists
+            const item = await Item.findOne({ name: itemName }).exec();
+            if (!item) {
+                interaction.reply({ content: `The item ${itemName} does not exist!\nType '/shop' for a list of available items.` });
+                return;
+            }
+            const embedsInfo = createItemEmbeds(item);
+            await interaction.reply({ embeds: embedsInfo });
+        }
+
+        //shop without option
+        const embedsList = createEmbeds();
+        await interaction.reply({ embeds: embedsList });
+    }
+
+};
