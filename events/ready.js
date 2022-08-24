87e2bb3a35b3d5c422bbbc128df1c61bb8eceb51
Michael <kuechler.michael03@gmail.com>
2022-08-24 10:59:55 +0200

    add Expelisa role

--- events/ready.js
+++ events/ready.js
@@ -1,18 +1,26 @@
 const mongoose = require('mongoose');
 const { checkForUpdates } = require('../controller/updateController');
-
+const Guild = require('./../schemas/Guild.js');
 //connect to db
 module.exports = {
     name: 'ready',
     once: true,
     async execute(client) {
-        mongoose.connect(process.env.MONGO_URI).then(() => {
-
+        mongoose.connect(process.env.MONGO_URI).then(async () => {
             console.log("connected to database");
+            console.log(`Ready! Logged in as ${client.user.tag}`);
+            checkForUpdates(client);
+            //create Expelsia role 
+            await client.guilds.cache.forEach(async g => {
+                const guild = await Guild.findOne({ id: g.id }).exec();
+                if (guild.allowsRoles) {
+                    if (!g.roles.cache.find(role => role.name === "Expelsia")) {
+                        g.roles.create({ name: 'Expelsia', color: '#43572c', mentionable: true, reason: 'members of Expelsia -Traider Naki bot' });
+                    }
+                }
+            });
         }).catch((err) => {
             console.log(err);
         })
-        console.log(`Ready! Logged in as ${client.user.tag}`);
-        checkForUpdates(client);
     },
 };
