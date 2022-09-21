0983a8b11ded73763af17f23c8040e3ab3795238
Michael <kuechler.michael03@gmail.com>
2022-09-21 19:37:23 +0200

    prepare fight

--- commands/challenge.js
+++ commands/challenge.js
@@ -1,9 +1,45 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageActionRow, MessageButton } = require('discord.js');
 const User = require('./../User.js');
-const emojis  = require('./../emojis.json');
+const emojis = require('./../emojis.json');
 
+const targetFilter = (int) => {
+    console.log(`filter for target id`);
+    if (int.user.id === target.discord_id) {
+        if (int.customId === "acceptChallenge" || int.customId === "denyChallenge") {
+            return true;
+        }
+        else {
+            return;
+        }
+    }
+    return int.reply({ content: `You can't use this button!`, ephemeral: true });
+};
+const soulstoneFilter = (int) => {
+    console.log(`filter for soulstone selection`);
+    if (int.user.id === interaction.user.id) {
+        const x = Number(int.customId)
+        if (x && x > 99) {
+            return true;
+        }
+        else {
+            return;
+        }
+    }
+    return int.reply({ content: `You can't use this button!`, ephemeral: true });
+};
 
+async function simulateFight(interaction, playerA, playerB) {
+    const results = {};
+    //create an embed as follow up
+
+    //make a 10sec counter before fight start
+
+    //every second take one round until someone dies
+
+    //return results
+    return results;
+}
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('challenge')
@@ -16,22 +52,14 @@ module.exports = {
     ,
     async execute(interaction) {
 
-        const targetFilter = (int) => {
-            console.log(`filter for target id`);
-            if (int.user.id === target.discord_id) {
-                return true;
-            }
-            return int.reply({ content: `You can't use this button!`, ephemeral: true });
-        };
-        const soulstoneFilter = (int) => {
-            console.log(`filter for soulstone selection`);
-            if (int.user.id === interaction.user.id) {
-                return true;
-            }
-            return int.reply({ content: `You can't use this button!`, ephemeral: true });
-        };
         //reply with buttons to select amount of soulstone
-        await interaction.deferReply();
+        try {
+            await interaction.deferReply();
+        }
+        catch (err) {
+            console.error(err);
+            return;
+        }
         const row = new MessageActionRow()
             .addComponents(
                 new MessageButton()
@@ -56,11 +84,45 @@ module.exports = {
             console.error(err);
             return;
         }
-        if (challenger.status != 'idle' && target.status != 'idle') {
-            await interaction.editReply({ content: `In order to challenge ${target.username} both of you must be idle!` });
-            return;
+        if (!challenger) {
+            //user not found
+            try {
+                await interaction.editReply({ content: `user not found! Type **/start** if you are not already in Expelsia.` });
+                return;
+            }
+            catch (err) {
+                console.error(err);
+                return;
+            }
         }
-        await interaction.editReply({ content: `Choose the amount of soulstone you want to bet.`, components: [row] })
+        if (!target) {
+            //user not found
+            try {
+                await interaction.editReply({ content: `Your enemy does not exist in Expelsia. Type **/leaderboard** to find possible enemies.` });
+                return;
+            }
+            catch (err) {
+                console.error(err);
+                return;
+            }
+        }
+        if (challenger.status != 'idle' && target.status != 'idle') {
+            //one user not available
+            try {
+                await interaction.editReply({ content: `In order to challenge ${target.username} both of you must be idle!` });
+                return;
+            }
+            catch (err) {
+                console.error(err);
+                return;
+            }
+        }
+        try {
+            await interaction.editReply({ content: `Choose the amount of soulstone you want to bet.`, components: [row] })
+        }
+        catch (err) {
+            console.error(err);
+        }
         const betCollector = interaction.channel.createMessageComponentCollector({
             filter: soulstoneFilter,
             time: 120000
@@ -68,70 +130,102 @@ module.exports = {
         betCollector.on('collect', async i => {
             betCollector.stop();
             console.log(`betcollector:)`)
-            await i.deferReply();
-            await interaction.editReply({ components: [] });
             try {
-                const chosenBet = Number(i.customId);
-                if (!Number.isInteger(chosenBet)) {
-                    await i.editReply({ content: `Something went wrong with the soulstone selection` });
-                    return;
-                }
-                //check if both user have enough soulstone
-                if (challenger.soulstones < chosenBet || target.soulstones < chosenBet) {
-                    console.log(`not enough soulstones`);
-                    await i.editReply({ content: `You or your enemy does not have enough soulstones ${emojis.soulstone}!` });
-                    return;
-                }
-                const challengeRow = new MessageActionRow()
-                    .addComponents(
-                        new MessageButton()
-                            .setCustomId(`yes`)
-                            .setLabel(`Yes`)
-                            .setStyle(`PRIMARY`),
-                        new MessageButton()
-                            .setCustomId(`no`)
-                            .setLabel(`No`)
-                            .setStyle(`PRIMARY`)
-                    );
-                await i.editReply({ content: `**PVP CHALLENGE**\n<@${target.discord_id}> is challenged by ${challenger.username}. Do you want to accept the challenge and fight against ${challenger.username}?\n*You must be idle to accept the challenge*`, components: [challengeRow] });
-                const answerCollector = interaction.channel.createMessageComponentCollector({
-                    filter: targetFilter
-                });
-                answerCollector.on('collect', async int => {
-                    console.log(`answer collector`);
-                    answerCollector.stop();
-                    await int.deferReply();
-                    try {
-                        await i.editReply({components: []});
-                        if (int.customId === "no") {
-                            //deny challenge
-                            await int.editReply({ content: `${target.username} denied the challenge of ${challenger.username}` });
-                            return;
-                        }
-                        //check if both users are still idle and have enough soulstone
-                        if(challenger.status !='idle' || target.status !='idle'){
-                            await int.editReply({content:`Both opponents must be idle to fight against each other!`});
-                            return;
-                        }
-                        if(challenger.soulstones < chosenBet || target.soulstones < chosenBet){
-                            await int.editReply({content: `Both opponents must have equal or more than ${chosenBet}${emojis.soulstone}!`});
-                            return;
-                        }
-                        await int.editReply({content:`${target.username} accepted the challenge of ${challenger.username}. Let the battle begin!`});
-                    }
-                    catch (err) {
-                        console.error(err);
-                        return;
-                    }
-                    //follow up int with battle
-                });
+                await i.deferReply();
+                await interaction.editReply({ components: [] });
             }
             catch (err) {
                 console.error(err);
+                return;
             }
-        });
-        //fight if enemy accepted and both are able to fight
+            const chosenBet = Number(i.customId);
+            if (!Number.isInteger(chosenBet)) {
+                try {
+                    await i.editReply({ content: `Something went wrong with the soulstone selection` });
+                }
+                catch (err) {
+                    console.error(err)
+                }
+                return;
+            }
+            //check if both user have enough soulstone
+            if (challenger.soulstones < chosenBet || target.soulstones < chosenBet) {
+                console.log(`not enough soulstones`);
+                try {
+                    await i.editReply({ content: `You or your enemy does not have enough soulstones ${emojis.soulstone}!` });
+                }
+                catch (err) {
+                    console.error(err);
+                }
+                return;
+            }
+            const challengeRow = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setCustomId(`acceptChallenge`)
+                        .setLabel(`Yes`)
+                        .setStyle(`PRIMARY`),
+                    new MessageButton()
+                        .setCustomId(`denyChallenge`)
+                        .setLabel(`No`)
+                        .setStyle(`PRIMARY`)
+                );
+            try {
+                await i.editReply({ content: `**PVP CHALLENGE**\n<@${target.discord_id}> is challenged by ${challenger.username}. Do you want to accept the challenge and fight against ${challenger.username}?\n*You must be idle to accept the challenge*`, components: [challengeRow] });
+            }
+            catch (err) {
+                console.error(err);
+                return;
+            }
+            const answerCollector = interaction.channel.createMessageComponentCollector({
+                filter: targetFilter
+            });
+            answerCollector.on('collect', async int => {
+                console.log(`answer collector`);
+                answerCollector.stop();
+                try {
+                    await int.deferReply();
+                    await i.editReply({ components: [] });
+                }
+                catch (err) {
+                    console.error(err);
+                    return;
+                }
+                try {
+                    if (int.customId === "denyChallenge") {
+                        //deny challenge
+                        await int.editReply({ content: `${target.username} denied the challenge of ${challenger.username}` });
+                        return;
+                    }
+                    //check if both users are still idle and have enough soulstone
+                    if (challenger.status != 'idle' || target.status != 'idle') {
+                        await int.editReply({ content: `Both opponents must be idle to fight against each other!` });
+                        return;
+                    }
+                    if (challenger.soulstones < chosenBet || target.soulstones < chosenBet) {
+                        await int.editReply({ content: `Both opponents must have equal or more than ${chosenBet}${emojis.soulstone}!` });
+                        return;
+                    }
+                    await int.editReply({ content: `${target.username} accepted the challenge of ${challenger.username}. Let the battle begin!` });
+                }
+                catch (err) {
+                    console.error(err);
+                    return;
+                }
+                //start fight
+                try {
+                    const results = await simulateFight(int, challenger, target);
+                }
+                catch (err) {
+                    console.error(err);
+                    return;
+                }
+                //TODO: make a complete battle report to download
 
-        //
+                //update both user: health, status, xp, soulstone
+            });
+
+        });
+
     }
 }
